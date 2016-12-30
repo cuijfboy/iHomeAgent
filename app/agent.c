26721abf09c1bf0f132423435499234ce5d53909
cuijunfeng <cuijf@haierubic.com>
2016-12-30 17:22:25 +0800

    refactor protocol [WIP]

--- app/agent.c
+++ app/agent.c
@@ -10,18 +10,18 @@
 #include "gzll.h"
 
 #define UART_ADDR						0xFF
-#define UART_PIP						0xFF
-#define UART_SEQ 						0xFF
+#define UART_PIP						0x3F
 
 #define MY_ADDR							0x0A
 
 #define PMW 								0x10
-
 #define NV_PAGE_ADDR				32
 #define NV_ADDR_ADDR				0xFA00
 
-#define CMD_ACK_FLAG        0x80
-#define CMD_UART_FLAG				0x40
+#define STA_ACK_FLAG        0x80
+#define STA_UART_FLAG		0x40
+#define STA_PIP_MASK		0x3F
+
 #define CMD_NOP             0x00
 #define CMD_GET_PORT	    	0x01
 #define CMD_SET_PORT	    	0x02
@@ -39,18 +39,16 @@
 #define UART_MSG						0x01
 #define NRF_MSG							0x02
 
-static struct uart_msg_fifo 	*uart_msg_rx_fifo, *uart_msg_tx_fifo; 
+static struct uart_msg_fifo 	*uart_msg_rx_fifo, *uart_msg_tx_fifo;
 static struct nrf_msg_fifo 	*nrf_msg_rx_fifo, *nrf_msg_tx_fifo;
 
-static uint8_t /*addrs[6],*/ my_addr;
-static uint8_t msg_seq;
+static uint8_t my_addr;
 
 void agent_set_my_addr(void);
-void agent_task(uint8_t msg_type, uint8_t *msg, uint8_t len);
-uint8_t agent_atcion( uint8_t src, uint8_t dst, uint8_t pip, uint8_t seq, 
-											uint8_t *cmd, uint8_t cmd_len, 
-											uint8_t *res_cmd, uint8_t* res_cmd_len);
-uint8_t get_msg_seq(void);
+void agent_task(uint8_t type, uint8_t *in, uint8_t in_len);
+uint8_t agent_atcion( uint8_t src, uint8_t dst, uint8_t sta, uint8_t seq,
+											uint8_t *dat, uint8_t dat_len,
+											uint8_t *out_dat, uint8_t* out_dat_len)
 
 void agent_init(struct uart_msg_fifo *uart_msg_rx_fifo_tmp,
 								struct uart_msg_fifo *uart_msg_tx_fifo_tmp,
@@ -61,7 +59,7 @@ void agent_init(struct uart_msg_fifo *uart_msg_rx_fifo_tmp,
 	uart_msg_tx_fifo = uart_msg_tx_fifo_tmp;
 	nrf_msg_rx_fifo = nrf_msg_rx_fifo_tmp;
 	nrf_msg_tx_fifo = nrf_msg_tx_fifo_tmp;
-	
+
 	PCON &= ~PMW;
 	my_addr = hal_flash_byte_read(NV_ADDR_ADDR);
 	if(my_addr == 0xFF)
@@ -80,159 +78,138 @@ void agent_set_my_addr(void)
 void agent_process(void)
 {
 	uint8_t msg[MSG_SIZE], len, pip;
-	
+
 	while(!uart_msg_fifo_empty(uart_msg_rx_fifo) && !nrf_msg_fifo_full(nrf_msg_tx_fifo))
 	{
 		uart_msg_fifo_poll(uart_msg_rx_fifo, msg, &len);
 		agent_task(UART_MSG, msg, len);
 		//nrf_msg_fifo_offer(nrf_msg_tx_fifo, msg, len, 0);
 	}
-	
+
 	while(!nrf_msg_fifo_empty(nrf_msg_rx_fifo) && !uart_msg_fifo_full(uart_msg_tx_fifo))
 	{
 		nrf_msg_fifo_poll(nrf_msg_rx_fifo, msg, &len, &pip);
 		//uart_put_data(msg, len);
 		agent_task(NRF_MSG, msg, len);
 	}
-}					
+}
 
-void agent_task(uint8_t msg_type, uint8_t *msg, uint8_t len)
+void agent_task(uint8_t type, uint8_t *in, uint8_t in_len)
 {
-	uint8_t src, dst, pip, seq, *cmd, cmd_len;
-	uint8_t res[MSG_SIZE], res_len, *res_cmd, res_cmd_len, res_code;
+	uint8_t pip, out[MSG_SIZE], out_len, *out_dat, out_dat_len, res;
 
-	src = msg[0];
-	dst = msg[1];
-	pip = msg[2];
-	seq = msg[3];
-	cmd = msg + 4;
-	cmd_len = len - 4;
-	
-	res_cmd = res + 4;
-	
-	res_code = agent_atcion(src, dst, pip, seq, 
-													cmd, cmd_len, res_cmd, &res_cmd_len);
-	
-	if(res_code == RES_NOP)
+	pip = sta & STA_PIP_MASK;
+	out_dat = out + 4;
+
+	// src, dst, sta, seq, dat, dat_len, out_dat, out_dat_len
+	res = agent_action(in[0], in[1], in[2], in[3], in + 4, in_len - 4, out_dat, &out_dat_len);
+
+	if(res == RES_NOP)
 		return;
-	
-	if(res_code & RES_FWD_DST)
+
+	if(res & RES_FWD_DST)
 	{
-		if(src == UART_ADDR)
-		{	
-			msg[0] = my_addr;
-			cmd[0] |= CMD_UART_FLAG;
+		if(type == UART_MSG)
+		{
+			// in_src = my_addr
+			in[0] = my_addr;
+			// set uart flag to in_sta
+			in[2] |= STA_UART_FLAG;
 		}
-		if(seq == UART_SEQ)
-			msg[3] = get_msg_seq();
-	
-		nrf_msg_fifo_offer(nrf_msg_tx_fifo, msg, len, pip);
+
+		nrf_msg_fifo_offer(nrf_msg_tx_fifo, in, in_len, pip);
 	}
-	
+
 	if(res_code & RES_FWD_UART)
-	{
-		uart_msg_fifo_offer(uart_msg_tx_fifo, msg, len);
-	}
-	
+		uart_msg_fifo_offer(uart_msg_tx_fifo, in, in_len);
+
 	if(res_code & RES_REP_SRC)
 	{
-		res_cmd[0] |= CMD_ACK_FLAG;
-		res[0] = my_addr;
-		res[1] = src;
-		res[2] = pip;
-		res[3] = seq;
-		res_len = res_cmd_len + 4;
-		
-		if(src == UART_ADDR)
-			uart_msg_fifo_offer(uart_msg_tx_fifo, res, res_len);
+		// out_src = my_addr
+		out[0] = my_addr;
+		// out_dst = in_src
+		out[1] = in[0];
+		// out_sta = in_sta & ack
+		out[2] = in[2] | STA_ACK_FLAG;
+		// out_seq = in_seq
+		out[3] = in[3];
+		out_len = out_dat_len + 4;
+
+		if(type == UART_MSG)
+			uart_msg_fifo_offer(uart_msg_tx_fifo, out, out_len);
 		else
-			nrf_msg_fifo_offer(nrf_msg_tx_fifo, res, res_len, pip);
+			nrf_msg_fifo_offer(nrf_msg_tx_fifo, out, out_len, pip);
 	}
 }
 
-uint8_t agent_atcion( uint8_t src, uint8_t dst, uint8_t pip, uint8_t seq, 
-											uint8_t *cmd, uint8_t cmd_len, 
-											uint8_t *res_cmd, uint8_t* res_cmd_len)
+uint8_t agent_atcion( uint8_t src, uint8_t dst, uint8_t sta, uint8_t seq,
+											uint8_t *dat, uint8_t dat_len,
+											uint8_t *out_dat, uint8_t* out_dat_len)
 {
-	uint8_t cmd_val;
-	
 	if(dst != my_addr && (dst != UART_ADDR || src != UART_ADDR))
 		return RES_FWD_DST;
-	
-	if(cmd[0] & CMD_ACK_FLAG)
-		if(cmd[0] & CMD_UART_FLAG)
+
+	if(sta & STA_ACK_FLAG)
+		if(sta & STA_UART_FLAG)
 			return RES_FWD_UART;
 		else
 			return RES_NOP;
-	
-	cmd_val = cmd[0] & ~CMD_UART_FLAG & ~CMD_ACK_FLAG;
-		
-	if(cmd_val == CMD_FWD)
+
+	if(dat[0] == CMD_FWD)
 		return RES_FWD_UART;
-	
-	res_cmd[0] = cmd[0];
-	
-	switch(cmd_val)
+
+	out_dat[0] = dat[0];
+
+	switch(dat[0])
 	{
-		case CMD_FWD:
-			return RES_FWD_UART;
-		
 		case CMD_NOP:
-			*res_cmd_len = cmd_len;
+			*out_dat_len = dat_len;
 			break;
-		
+
 		case CMD_SET_PORT:
-			PORT_0 |= cmd[1] &  cmd[2] &  PORT_0_MASK;
-			PORT_0 &= cmd[1] | ~cmd[2] | ~PORT_0_MASK;
-			PORT_1 |= cmd[3] &  cmd[4] &  PORT_1_MASK;
-			PORT_1 &= cmd[3] | ~cmd[4] | ~PORT_1_MASK;
-		
+			PORT_0 |= dat[1] &  dat[2] &  PORT_0_MASK;
+			PORT_0 &= dat[1] | ~dat[2] | ~PORT_0_MASK;
+			PORT_1 |= dat[3] &  dat[4] &  PORT_1_MASK;
+			PORT_1 &= dat[3] | ~dat[4] | ~PORT_1_MASK;
+
 		case CMD_GET_PORT:
-			res_cmd[1] = PORT_0      & cmd[2];
-			res_cmd[2] = PORT_0_MASK & cmd[2];
-			res_cmd[3] = PORT_1      & cmd[4];
-			res_cmd[4] = PORT_1_MASK & cmd[4];
-		  *res_cmd_len = 5;
+			out_dat[1] = PORT_0      & dat[2];
+			out_dat[2] = PORT_0_MASK & dat[2];
+			out_dat[3] = PORT_1      & dat[4];
+			out_dat[4] = PORT_1_MASK & dat[4];
+			*out_dat_len = 5;
 			break;
-		
+
 		case CMD_SET_DIR:
-			PORT_0_DIR |= cmd[1] &  cmd[2] &  PORT_0_MASK;
-			PORT_0_DIR &= cmd[1] | ~cmd[2] | ~PORT_0_MASK; 
-			PORT_1_DIR |= cmd[3] &  cmd[4] &  PORT_1_MASK;
-			PORT_1_DIR &= cmd[3] | ~cmd[4] | ~PORT_1_MASK; 
-		
+			PORT_0_DIR |= dat[1] &  dat[2] &  PORT_0_MASK;
+			PORT_0_DIR &= dat[1] | ~dat[2] | ~PORT_0_MASK;
+			PORT_1_DIR |= dat[3] &  dat[4] &  PORT_1_MASK;
+			PORT_1_DIR &= dat[3] | ~dat[4] | ~PORT_1_MASK;
+
 		case CMD_GET_DIR:
-			res_cmd[1] = PORT_0_DIR  & cmd[2];
-			res_cmd[2] = PORT_0_MASK & cmd[2];
-			res_cmd[3] = PORT_1_DIR  & cmd[4];
-			res_cmd[4] = PORT_1_MASK & cmd[4];
-			*res_cmd_len = 5;
+			out_dat[1] = PORT_0_DIR  & dat[2];
+			out_dat[2] = PORT_0_MASK & dat[2];
+			out_dat[3] = PORT_1_DIR  & dat[4];
+			out_dat[4] = PORT_1_MASK & dat[4];
+			*out_dat_len = 5;
 			break;
-		
+
 		case CMD_SET_ADDR:
 			PCON &= ~PMW;
 			hal_flash_page_erase(NV_PAGE_ADDR);
-		  hal_flash_byte_write(NV_ADDR_ADDR, cmd[1]);
-		  my_addr = hal_flash_byte_read(NV_ADDR_ADDR);
-		  agent_set_my_addr();
-		
+			hal_flash_byte_write(NV_ADDR_ADDR, dat[1]);
+			my_addr = hal_flash_byte_read(NV_ADDR_ADDR);
+			agent_set_my_addr();
+
 		case CMD_GET_ADDR:
-			res_cmd[1] = my_addr;
-			*res_cmd_len = 2;
+			out_dat[1] = my_addr;
+			*out_dat_len = 2;
 			break;
-		
+
 		default:
-			*res_cmd_len = 1;
+			*out_dat_len = 1;
 			break;
 	}
-	
-	return RES_REP_SRC;
-}
 
-uint8_t get_msg_seq(void)
-{
-	msg_seq++;
-	if(msg_seq == UART_SEQ)
-		msg_seq = 0;
-	return msg_seq;
+	return RES_REP_SRC;
 }
